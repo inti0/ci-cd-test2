bca0848d1c50914750e386dd80123551a0c68b34
inti0 <sonin223@gmail.com>
2025-10-24 12:18:36 +0900

    init

---
+++ src/main/java/com/cicdtest2/CiCdTest2Application.java
@@ -0,0 +1,13 @@
+package com.cicdtest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdTest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cicdtest2/CiCdTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicdtest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
